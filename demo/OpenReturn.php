33239d4fb48dea8bcb1377e67531bd9dac37ba70
hetao <hetao@hetao.name>
2025-12-15 16:15:51 +0800

    更新回调

--- demo/OpenReturn.php
+++ demo/OpenReturn.php
@@ -8,22 +8,22 @@
  */
 
 try {
-    require_once '../Init/init.php';
-    Log::LogWirte("===================接收开户异步通知========================");
-    $EndataContent =  isset($_REQUEST["data_content"])?$_REQUEST["data_content"]:die("No parameters are received [data_content]");
-    $MemberID=$_REQUEST['member_id'];//商户号
-    $TerminalID =$_REQUEST['terminal_id'];//商户终端号
+    require_once 'init.php';
+    Baofu\Util\Log::LogWirte("===================接收开户异步通知========================");
+    $EndataContent =  isset($_REQUEST["data_content"]) ? $_REQUEST["data_content"] : die("No parameters are received [data_content]");
+    $MemberID = $_REQUEST['member_id']; //商户号
+    $TerminalID = $_REQUEST['terminal_id']; //商户终端号
 
-    Log::LogWirte("异步通知原文：".$EndataContent);
-    $ReturnDecode = NewRsaUtil::decrptByPub($EndataContent,$GLOBALS["CER_FILE_PATH"]);//解密返回的报文
-    Log::LogWirte("异步通知解密原文：".$ReturnDecode);
-    
+    Baofu\Util\Log::LogWirte("异步通知原文：" . $EndataContent);
+    $ReturnDecode = Baofu\Security\RSA::decryptByCERFile($EndataContent, $GLOBALS["CER_FILE_PATH"]); //解密返回的报文
+    Baofu\Util\Log::LogWirte("异步通知解密原文：" . $ReturnDecode);
+
     /**
      * 业务处理
      */
-    
-    echo "OK";//返回OK与业务无关，只表示收到异步通知
-    
+
+    echo "OK"; //返回OK与业务无关，只表示收到异步通知
+
 } catch (Exception $ex) {
     echo "OK";
 }
--- demo/SyncUploadFile.php
+++ demo/SyncUploadFile.php
@@ -6,36 +6,36 @@
  * @datetime 2025/05/05
  */
 try {
-    require_once "../Init/init.php";
+    require_once 'init.php';
     $OrderType = "0";
-    $filezip = "H:\\pic\\idphoto.zip";
+    $filezip = "/Users/hetal/Downloads/test.zip";
 
-    $PostParm =[];// 提交参数
+    $PostParm = []; // 提交参数
     $PostParm["memberId"] = $GLOBALS["Member_Id"];
     $PostParm["terminalId"] = $GLOBALS["Terminal_Id"];
     $PostParm["orderType"] = $OrderType;
 
     $DataParm = [];
     $DataParm["accType"] = "2";
-    $DataParm["transSerialNo"] = Tools::getTransid("TSN");
+    $DataParm["transSerialNo"] = Baofu\Util\Tools::getTransid("TSN");
     $DataParm["businessParams"] = "LN17464143403681"; // Ordertype 0 :自定义唯一标识，示例：登录号
     //$DataParm["noticeUrl"] = ""; // 回调地址
-    Log::EchoFormat("上传流水号：".$DataParm["transSerialNo"]);
+    Baofu\Util\Log::EchoFormat("上传流水号：" . $DataParm["transSerialNo"]);
     $File101 = [];
     $File101["fileType"] = "101";
-    $File101["fileName"] = "business_license";
+    $File101["fileName"] = "business_license.png";
 
     $File102 = [];
     $File102["fileType"] = "102";
-    $File102["fileName"] = "acc_open_license";
+    $File102["fileName"] = "acc_open_license.png";
 
     $File104 = [];
     $File104["fileType"] = "104";
-    $File104["fileName"] = "id_front";
+    $File104["fileName"] = "id_front.png";
 
     $File111 = [];
     $File111["fileType"] = "111";
-    $File111["fileName"] = "id_reverse";
+    $File111["fileName"] = "id_reverse.png";
 
     $FileNameMaps = [];
     $FileNameMaps[] = $File101;
@@ -45,31 +45,43 @@ try {
 
     $DataParm["fileNameMap"] = $FileNameMaps;
 
-    Log::EchoFormat("上传文件：".realpath($filezip).",".mime_content_type($filezip).",".basename($filezip));
+    Baofu\Util\Log::EchoFormat("上传文件：" . realpath($filezip) . "," . mime_content_type($filezip) . "," . basename($filezip));
     $sufJson = json_encode($DataParm);
-    Log::EchoFormat("JSON序列化：".$sufJson);
-    $PostParm["content"] = NewRsaUtil::encryptByPfx($sufJson,$GLOBALS["PFX_FILE_PATH"],$GLOBALS["Key_Pwd"]);
-    Log::EchoFormat("请求参数".json_encode($PostParm));
+    Baofu\Util\Log::EchoFormat("JSON序列化：" . $sufJson);
+    $PostParm["content"] = Baofu\Security\RSA::encryptByPFXFile($sufJson, $GLOBALS["PFX_FILE_PATH"], $GLOBALS["Key_Pwd"]);
+    Baofu\Util\Log::EchoFormat("请求参数" . json_encode($PostParm));
 
-    if(!empty($filezip)){// 文件上传
-        if(!file_exists($filezip))  throw new \Exception("上传文件不存在！路径：".$filezip);
-        $PostParm["file"] = new CURLFile(realpath($filezip),mime_content_type($filezip),basename($filezip));
+    $multipart = [];
+    foreach ($PostParm as $k => $v) {
+        $multipart[] = [
+            "name" => $k,
+            "contents" => $v,
+        ];
     }
+    $multipart[] = [
+        'name'     => 'file', // The name of the form field the API expects
+        'contents' => fopen($filezip, 'r'), // Use fopen for efficient streaming
+        'filename' =>  basename($filezip), // Optional: provides a filename to the API
+    ];
+    print_r($multipart);
+    $client = new \GuzzleHttp\Client();
+    $res = $client->request('POST', "https://vgw.baofoo.com/baofu-upload-trade/trade/syncUploadFile", [
+        'multipart' => $multipart,
+    ]);
 
-    $returnstr = HttpClient::MultiPost($PostParm,"https://vgw.baofoo.com/baofu-upload-trade/trade/syncUploadFile",$filezip);
-    Log::EchoFormat("返回报文：".$returnstr);
-    if(empty($returnstr)) throw new Exception("请求返回为空！");
-    $debyresult = NewRsaUtil::decrptByPub($returnstr,$GLOBALS["CER_FILE_PATH"]);
-    Log::EchoFormat("解密：".$debyresult);
-    $resultObj = json_decode($debyresult,true);
-    if($resultObj["success"])
-    {
-        Log::EchoFormat("流水号：".$resultObj["result"]["transSerialNo"]);
-        foreach ($resultObj["result"]["dfsFileNotifyList"] as $item)
-        {
-            Log::EchoFormat("文件：".json_encode($item));
+    $returnstr = (string)$res->getBody();
+
+    Baofu\Util\Log::EchoFormat("返回报文：" . $returnstr);
+    if (empty($returnstr)) throw new Exception("请求返回为空！");
+    $debyresult = Baofu\Security\RSA::decryptByCERFile($returnstr, $GLOBALS["CER_FILE_PATH"]);
+    Baofu\Util\Log::EchoFormat("解密：" . $debyresult);
+    $resultObj = json_decode($debyresult, true);
+    if ($resultObj["success"]) {
+        Baofu\Util\Log::EchoFormat("流水号：" . $resultObj["result"]["transSerialNo"]);
+        foreach ($resultObj["result"]["dfsFileNotifyList"] as $item) {
+            Baofu\Util\Log::EchoFormat("文件：" . json_encode($item));
         }
     }
-}catch (Exception $ex){
+} catch (Exception $ex) {
     echo $ex->getMessage();
 }
